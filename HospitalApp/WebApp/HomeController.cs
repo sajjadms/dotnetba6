7193c3c406f340d81dc85cd080823ebe37324be4
sajjad@gs-ms.com <sajjad@gs-ms.com>
2024-01-12 11:41:12 +0530

    First Commit Login Page

---
+++ HospitalApp/WebApp/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospitalApp/WebApp/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp
+{
+    public class LoginController : Controller
+    {
+        [HttpGet()]
+        public IActionResult Logon()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitLogin()
+        {
+            return RedirectToAction("Dashboard", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Logon");
+        }
+    }
+}
