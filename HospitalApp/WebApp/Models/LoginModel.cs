c7d4ff8b0287cf2b446af4e69707107688228637
sajjad@gs-ms.com <sajjad@gs-ms.com>
2024-01-29 11:19:23 +0530

    TAG HELPERS

---
+++ HospitalApp/WebApp/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Id")]     //<label asp-for ="Email" />  ->  <label>Email Id</label>
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        
+    }
+}
